9ffa1e683ef9775f7f917c6aab1681ef48020fbb
chedi <dayehchadi56@gmail.com>
2026-03-11 15:52:09 +0100

    refactor: remove webpack optimization from next.config and update build commands

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,16 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    // Optimize webpack config for build environments
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        runtimeChunk: 'single',
-      };
-    }
-    return config;
-  },
 };
 
 export default withNextIntl(nextConfig);
